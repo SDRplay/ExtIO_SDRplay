88243e4574277868642d175db48c064cb1abe771
SDRplay <info@sdrplay.com>
2024-05-12 07:50:18 +0100

    Add RSPdxR2 Support

--- README.md
+++ README.md
@@ -6,12 +6,14 @@ All of these plugins require the API 3.07 or above.
 
 Note: The RSP1B ExtIO plugin requires API 3.14 MINIMUM
 
+Note: The RSPdxR2 ExtIO plugin requires API 3.15 MINIMUM
+
 Note: The RSPdx ExtIO plugin DOES NOT, AND WILL NOT, SUPPORT HDR MODE. Not possible to do with just the ExtIO protocol.
 
 This source code is all provide as is without support.
 
 The Releases section will contain a zip file of just the ExtIO plugins.
 
-Go to https://www.sdrplay.com/downloads to get the relevant API
+Go to https://www.sdrplay.com/api to get the relevant API
 
 SDRplay
---
+++ RSPdxR2/src/ExtIO_SDRplay.h
@@ -0,0 +1,18 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the LIBSDRplay_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// LIBSDRplay_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+
+#ifdef LIBSDRplay_EXPORTS
+#define LIBSDRplay_API __declspec(dllexport)
+#else
+#define LIBSDRplay_API __declspec(dllimport)
+#endif
+
+extern HMODULE hInst;
+void LoadProfile(int);
+void LoadProfilesReg(HWND);
+void LoadFreqsReg(HWND);
+void UpdateProfile(void);
